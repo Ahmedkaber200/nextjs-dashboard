be763f7e2e097d30f125d1f88bba994bb33431cd
Ahmed Kaber <ahmedkaber200@gmail.com>
2025-02-19 19:33:49 +0500

    I have added the Latest Invoices page to the dashboard.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -68,6 +68,8 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
+      console.log(data);
+
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
--- app/ui/dashboard/page.tsx
+++ app/ui/dashboard/page.tsx
@@ -6,21 +6,20 @@ import {RevenueChart} from '@/app/ui/dashboard/revenue-chart';
 
 // import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 // import RevenueChart from '../ui/dashboard/revenue-chart';
-// import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import {LatestInvoices} from '@/app/ui/dashboard/latest-invoices';
 // import { lusitana } from '@/app/ui/fonts';
 // import { fetchRevenue } from '@/app/lib/data';
 import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
 
 export default async function Page() {
     const revenue = await fetchRevenue();
-    console.log(revenue);
-    return (<RevenueChart revenue={revenue}/>)
-    // const fetchrevenue = await RevenueChart();
-    // console.log(fetchrevenue);
-    // const latestInvoices = await fetchLatestInvoices();
+    const latestInvoices = await fetchLatestInvoices();
+    // console.log(revenue);
     // console.log(latestInvoices);
+    return (
+        <div>
+            <RevenueChart revenue={revenue} />
+            <LatestInvoices latestInvoices={latestInvoices} />
+        </div>
+    );
 }
-
-// export default function RevenueChart() {
-//   return <div>Chart</div>;
-// }
