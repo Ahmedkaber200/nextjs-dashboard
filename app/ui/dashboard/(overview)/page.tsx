e51e0ce602f2eb503c123192e5fe56d3af81a479
Ahmed Kaber <ahmedkaber200@gmail.com>
2025-03-03 17:58:52 +0500

    Learning in Partial Prerendering

--- app/ui/dashboard/(overview)/page.tsx
+++ app/ui/dashboard/(overview)/page.tsx
@@ -1,26 +1,24 @@
-import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data'; // Remove fetchLatestInvoices
-import { Suspense } from 'react';
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import { fetchCardData } from "@/app/lib/data"; // Remove fetchLatestInvoices
+import { Suspense } from "react";
 // import CardWrapper from '@/app/ui/dashboard/cards';
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
-} from '@/app/ui/skeletons';
+  CardsSkeleton,
+} from "@/app/ui/skeletons";
 // console.log('RevenueChartSkeleton');
 export default async function Page() {
-  // Remove `const latestInvoices = await fetchLatestInvoices()`
-  // console.log('Fetched Revenue Data:', revenue); // Data ko console par check karein
-
   const {
     numberOfInvoices,
     numberOfCustomers,
     totalPaidInvoices,
     totalPendingInvoices,
   } = await fetchCardData();
-  
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -37,13 +35,13 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
-        </Suspense>
-        <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
-        </Suspense>
-      </div>
+          <Suspense fallback={<RevenueChartSkeleton />}>
+            <RevenueChart />
+          </Suspense>
+          <Suspense fallback={<LatestInvoicesSkeleton />}>
+            <LatestInvoices />
+          </Suspense>
+        </div>
     </main>
   );
 }
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -23,9 +23,12 @@ export default async function CardWrapper() {
   } = await fetchCardData();
  
   return (
-    <>
+    <main>
+    <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
       {/* NOTE: Uncomment this code in Chapter 9 */}
-
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
@@ -34,7 +37,8 @@ export default async function CardWrapper() {
         value={numberOfCustomers}
         type="customers"
       />
-    </>
+      </div>
+    </main>
   );
 }
 
